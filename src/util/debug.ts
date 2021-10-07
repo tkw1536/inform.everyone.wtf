a4d492195ef7fa214e485b59a977be61db5aaafc
Tom Wiesing <tkw01536@gmail.com>
2021-10-07 11:30:15 +0200

    Update dependencies to latest

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -11,25 +11,25 @@ declare const process: {
  * Prints a warning to the info log
  * @param data Data to print to the info log
  */
-export function debug_info(...data: any) {
+export function debug_info(message: string, obj1?: any, obj2?: any, obj3?: any) {
     if (process.env.NODE_ENV === "production") return;
-    console.log(...data);
+    console.log(message, obj1, obj2);
 }
 
 /**
  * Prints a warning to the warning log
  * @param data Data to print to the debug log
  */
-export function debug_warn(...data: any) {
+export function debug_warn(message: string, obj1?: any, obj2?: any) {
     if (process.env.NODE_ENV === "production") return;
-    console.warn(...data);
+    console.warn(message, obj1, obj2);
 }
 
 /**
  * Prints a fatal error to the error log
  * @param data Data tro print to the error log
  */
-export function debug_fatal(...data: any) {
+export function debug_fatal(message: string, obj1?: any,  obj2?: any) {
     if (process.env.NODE_ENV === "production") return;
-    console.error(...data);
+    console.error(message, obj1, obj2);
 }
